96030878867bcf1cf7c703515485fe96c7bf6c1f
Christian E <christianelena92@gmail.com>
2023-08-15 09:50:21 -0500

    crate an input with a placeholder that takes in a key value and adds that data to an empty array, when the enter key is pressed push that text value out as a stirng and display it as a todo

---
+++ src/components/vue-todo-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{  todo }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo) {
+        this.todos.push(this.newTodo);
+        this.newTodo = ''; 
+    }
+  }
+}
+};
+</script>
